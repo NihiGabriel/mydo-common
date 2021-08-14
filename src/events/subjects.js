19cdb29925b1f527a41fd75fd93a80dd21ee5bae
Opeyemi1994 <adenijiopeyemi68@gmail.com>
2021-08-14 07:49:29 +0100

    fixed subject

--- src/events/subjects.js
+++ src/events/subjects.js
@@ -1,7 +1,12 @@
 const Subjects = {
 
     UserCreated: 'todo.user.created',
-    CountryFound: 'todo.country.found'
+    CountryFound: 'todo.country.found',
+    ReminderSend: 'todo.reminder.send',
+    ReminderCompleted: 'todo.reminder.completed',
+    ItemNudged: 'todo.item.nudged',
+    ItemAbandoned: 'todo.item.abandoned',
+    TodoCompleted: 'todo.todo.completed'
 }
 
 module.exports = Object.freeze(Subjects);
